a651f7443b101cf204e0f749f22397beffff59ac
falcon <you@example.com>
2024-06-18 17:16:49 +0300

    bro

--- src/disc.rs
+++ src/disc.rs
@@ -1,5 +1,6 @@
 
-pub fn length_of_last_word(s: String) -> i32 {
+pub fn length_of_last_word(s: String) -> i32
+{
     s.trim().split(' ').collect::<Vec<_>>().last().unwrap().len() as i32
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+
+pub mod disc;
+
+use crate::disc::length_of_last_word;
+
+
 fn main() {
     println!("Hello, world!");
+
+    length_of_last_word("sredtjnh".to_string());
+
+    let g: i32 = factorial(10);
+    println!("{g}");
+}
+
+fn factorial(x: i32) -> i32
+{
+    if x == 0 { 1 } else { x * factorial(x - 1) }
 }
