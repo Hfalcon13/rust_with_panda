689bd66a195e6afe579c0f818b04cc29c5d91da1
falcon <you@example.com>
2024-06-21 11:28:29 +0300

    added more

--- src/disc.rs
+++ src/disc.rs
@@ -1,36 +1,54 @@
 use std::{io::{self, Write}};
 
+use rand::Rng;
+
 
 pub fn length_of_last_word(s: String) -> i32
 {
-    s.trim().split(' ').collect::<Vec<_>>().last().unwrap().len() as i32
+	s.trim().split(' ').collect::<Vec<_>>().last().unwrap().len() as i32
 }
 
 fn read_line() -> String {
-    let mut input = String::new();
-    io::stdout().flush().unwrap();  // Ensure the prompt is displayed before reading input
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    input.trim().to_string()  // Trim the newline character and return the input
+	let mut input = String::new();
+	io::stdout().flush().unwrap();  // Ensure the prompt is displayed before reading input
+	io::stdin().read_line(&mut input).expect("Failed to read line");
+	input.trim().to_string()  // Trim the newline character and return the input
 }
 
-fn gen_targil_by_score(score: i32) -> Option<f64>
+fn gen_targil_by_score(score: i32) -> Option<i32>
 {
-    match score {
-        n if n < 10 => 
-        {
-            
-        }
-        _ => 
-        {
-            println!("bro ended the game");
-            None
-        }
-    }
+	let mut rng = rand::thread_rng();
+	match score {
+		n if n < 10 => 
+		{
+			let a = rng.gen_range(0..11);
+			let b = rng.gen_range(0..11);
+			println!("{} + {}", a, b);
+			Some(a + b)
+		}
+		_ => 
+		{
+			println!("bro ended the game");
+			None
+		}
+	}
 }
 
 pub fn math_game_main()
 {
-    println!("test")
+	let mut score = 0;
+	while true {
+		let result = gen_targil_by_score(score);
+		let user_result = read_line().parse::<i32>().unwrap();
+		if result.is_none()
+		{
+			break;
+		}
+		else
+		{
+			
+		}
+	}
 }
 
 
