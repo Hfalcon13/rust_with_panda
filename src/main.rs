1d2d3b6dfdb9bb45d40a25254468c08f3733655a
falcon <you@example.com>
2024-06-25 21:47:37 +0300

    "Added virtual-memory crate dependency and implemented run_ml function to execute machine code in a virtual memory environment"

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-use disc::math_game_main;
+
+//use disc::math_game_main;
+use virtual_memory::VirtualMemory;
 
 
 
 pub mod disc;
 
 
+fn run_ml(buf: &[u8], arg: u64) -> u64
+{
+    let mut memory = VirtualMemory::new(buf.len()).expect("failed to allocate rwx memory");
+    memory.copy_from_slice(buf);
+
+    let f: extern "C" fn(u64) -> u64 = unsafe { std::mem::transmute(memory.as_ptr()) };
+
+    return f(arg);
+}
 
 fn main() {
-    math_game_main()
+
+	let code = [0x6A, 0x01, 0x58, 0x48, 0x0F, 0xAF, 0xC1, 0x48, 0xFF, 0xC9, 0x75, 0xF7, 0xC3];
+
+    let res = run_ml(&code, 9);
+
+    println!("{}", res);
+
+    //math_game_main()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
